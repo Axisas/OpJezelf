42d366d9eebaccd581a5a99c2cb844d24dc28ca2
Axisas <78723986+Axisas@users.noreply.github.com>
2024-05-31 14:16:05 +0200

    Moved some eventlisteners, popup toggle now activates on restart

--- settings.js
+++ settings.js
@@ -1,23 +1,12 @@
 const settingsPopUp = document.getElementById("settingspopup");
 const popupBG = document.getElementById("popupbg");
 
-popupBG.addEventListener("pointerdown", togglePopUp);
-
-const settingsButton = document
-  .getElementById("settingsbutton")
-  .addEventListener("pointerdown", togglePopUp);
-
-const restart = document
-  .getElementById("restart")
-  .addEventListener("pointerdown", restartApp);
-
 function restartApp() {
   moneyValue = startingMoney;
   moneyText.innerHTML = moneyValue;
   moneyInputField.value = 0;
   checkBalance();
   clearInterval(changeMoney);
-  togglePopUp();
 }
 
 document.addEventListener("DOMContentLoaded", restartApp);
